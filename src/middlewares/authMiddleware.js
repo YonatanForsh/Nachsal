0a668b8f2da8e8b1360ed10265ce16bef7c3650b
Yonatan <yonatanforsh@gmail.com>
2024-10-07 11:41:53 +0300

    empty middleware

---
+++ src/middlewares/authMiddleware.js
@@ -0,0 +1,23 @@
+const onlyCommanders = async (req, res, next) => {
+    try {
+        
+    } catch (err) {
+        
+    }
+}
+
+
+const onlySoldiersAndCommanders = async (req, res, next) => {
+    try {
+        
+    } catch (err) {
+        
+    }
+}
+
+
+
+module.export = {
+    onlyCommanders,
+    onlySoldiersAndCommanders
+}

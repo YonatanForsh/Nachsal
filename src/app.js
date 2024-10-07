29ce532a9880be2476a3d3a647cedfce4b0fa5dc
Yonatan <yonatanforsh@gmail.com>
2024-10-07 17:22:00 +0300

    authentication done

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const exp = require("express")
 const dotenv = require("dotenv")
 const { connect } = require("mongoose")
 const { connectToMongo } = require("./config/dbConfig")
+const cookieParser = require("cookie-parser")
 
 
 const app = exp()
@@ -13,6 +14,7 @@ const port = process.env.PORT || 1415
 
 
 app.use(exp.json())
+app.use(cookieParser)
 app.use("/auth", require("./routes/authRouter"))
 app.use("/user", require("./routes/userRouter"))
 app.use("/greenEye", require("./routes/greenEyeRouter"))
--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -1,23 +1,34 @@
+const { verify } = require("jsonwebtoken")
+
 const onlyCommanders = async (req, res, next) => {
     try {
-        
+        const token = req.cookies.token
+        const userData = await jwt.verify(token, process.env.TOKEN_SECERET)
+        if(userData.role != "commander"){
+            req.status(403).send("youarn't a commander. you can't enter! HAHAHAHA")
+        }
+        req.user = userData
+        next()        
     } catch (err) {
-        
+        res.status(401).send(err.message)
     }
 }
 
 
 const onlySoldiersAndCommanders = async (req, res, next) => {
     try {
-        
+        const token = req.cookies.token
+        const userData = await jwt.verify(token, process.env.TOKEN_SECERET)
+        req.user = userData
+        next()        
     } catch (err) {
-        
+        res.status(401).send(err.message)
     }
 }
 
 
 
-module.export = {
+module.exports = {
     onlyCommanders,
     onlySoldiersAndCommanders
 }

afeac300c76f01a16998c0d0ac37c4d9d800dbcb
Yonatan <yonatanforsh@gmail.com>
2024-10-07 11:28:44 +0300

    empty controllers created

---
+++ src/controllers/greenEyeController.js
@@ -0,0 +1,52 @@
+const createPoll = async (req, res) => {
+    try {
+        
+    } catch (err) {
+        
+    }
+}
+
+const getNotification = async (req, res) => {
+    try {
+        
+    } catch (err) {
+        
+    }
+}
+
+
+const getPollById = async (req, res) => {
+    try {
+        
+    } catch (err) {
+        
+    }
+}
+
+const reply = async (req, res) => {
+    try {
+        
+    } catch (err) {
+        
+    }
+}
+
+
+const deletePoll = async (req, res) => {
+    try {
+        
+    } catch (err) {
+        
+    }
+}
+
+
+
+
+module.export = {
+    createPoll,
+    getNotification,
+    getPollById,
+    reply,
+    deletePoll
+}
---
+++ src/controllers/userController.js
@@ -0,0 +1,31 @@
+const register = async (req, res) => {
+    try {
+        
+    } catch (err) {
+        
+    }
+}
+
+const getProfile = async (req, res) => {
+    try {
+        
+    } catch (err) {
+        
+    }
+}
+
+
+const setSettings = async (req, res) => {
+    try {
+        
+    } catch (err) {
+        
+    }
+}
+
+
+module.export = {
+    getProfile,
+    register,
+    setSettings
+}

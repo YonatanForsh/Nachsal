8ee9e6bdd1e523fda48a688b84356216c6ae1fbd
Yonatan <yonatanforsh@gmail.com>
2024-10-07 10:31:25 +0300

    create and connect routers files

--- src/app.js
+++ src/app.js
@@ -6,6 +6,11 @@ dotenv.config()
 
 const port = process.env.PORT || 1415
 
+app.use(exp.json())
+app.use("/auth", require("./routes/authRouter"))
+app.use("/user", require("./routes/userRouter"))
+app.use("/greenEye", require("./routes/greenEyeRouter"))
+
 
 app.listen(port, () => console.log("Server running on: " + port))
 
